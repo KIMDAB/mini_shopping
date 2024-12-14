05a30d1479967b7ce25de9ba7e46010342dbdedf
KIMDAB <kdabin111@naver.com>
2024-12-14 22:07:08 +0900

    homepage 변경

--- src/main/java/com/example/mini_shopping/HomeContoller.java
+++ src/main/java/com/example/mini_shopping/HomeContoller.java
@@ -1,16 +1,26 @@
 package com.example.mini_shopping;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class HomeContoller {
 
-    @GetMapping({"/", "/home"})
-    public String home(){
-        log.info("index page");
+    @GetMapping({"/", "/home", ""})
+    public String home(HttpSession session, Model model){
+        log.info("homepage");
+
+        String id = (String) session.getAttribute("id");
+        log.info("id:{}", id);
+
+        model.addAttribute("session", session);
+        model.addAttribute("id", id);
+
 
         return "index";
     }

0544425117c355f0762889af49fff5bd011e0607
KIMDAB <kdabin111@naver.com>
2024-12-30 00:37:29 +0900

    카카오페이 ready header

---
+++ src/main/java/com/example/mini_shopping/naver/KakaoPayProperties.java
@@ -0,0 +1,18 @@
+package com.example.mini_shopping.naver;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "kakaopay")
+@Getter
+@Setter
+public class KakaoPayProperties {
+
+    private String secretKey;
+    private String cid;
+
+}

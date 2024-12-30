aa4467869dcdf659312e3987ab79e71bf1e5973a
KIMDAB <kdabin111@naver.com>
2024-12-30 18:00:54 +0900

    회원 amount VO

---
+++ src/main/java/com/example/mini_shopping/kakao/Amount.java
@@ -0,0 +1,21 @@
+package com.example.mini_shopping.kakao;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Amount {
+
+    private int total;
+    private int tax_free;
+    private int tax;
+    private int point;
+    private int discount;
+    private int green_deposit;
+
+
+
+}
